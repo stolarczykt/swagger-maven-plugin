709afa02c50d2b45edfa06fcb81217df41f56923
Tomasz Stolarczyk <stolarczykt@gmail.com>
2014-09-11 11:28:37 +0200

    moving mapping logic to model mapper

--- src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/MavenDocumentSource.java
+++ src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/MavenDocumentSource.java
@@ -9,7 +9,6 @@ import com.wordnik.swagger.jaxrs.JaxrsApiReader;
 import com.wordnik.swagger.jaxrs.reader.DefaultJaxrsApiReader;
 import com.wordnik.swagger.model.*;
 import org.apache.maven.plugin.logging.Log;
-import scala.None;
 import scala.Option;
 import scala.collection.JavaConversions;
 import scala.collection.mutable.Buffer;
@@ -18,6 +17,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,6 +27,7 @@ import java.util.List;
  */
 public class MavenDocumentSource extends AbstractDocumentSource {
 	private final ApiSource apiSource;
+	private final NinjaToSwaggerModelMapper modelMapper;
 
 	public MavenDocumentSource(ApiSource apiSource, Log log) {
 		super(new LogAdapter(log),
@@ -36,6 +37,7 @@ public class MavenDocumentSource extends AbstractDocumentSource {
 		setBasePath(apiSource.getBasePath() + apiSource.getApiUri());
 		setApiInfo(apiSource.getApiInfo());
 		this.apiSource = apiSource;
+		this.modelMapper = new NinjaToSwaggerModelMapper();
 	}
 
 	@Override
@@ -43,10 +45,10 @@ public class MavenDocumentSource extends AbstractDocumentSource {
 		SwaggerConfig swaggerConfig = new SwaggerConfig();
 		swaggerConfig.setApiVersion(apiSource.getApiVersion());
 		swaggerConfig.setSwaggerVersion(SwaggerSpec.version());
-		List<ApiListingReference> apiListingReferences = new ArrayList<ApiListingReference>();
+		List<ApiListingReference> apiListingReferences = new ArrayList<>();
 
-		List<AuthorizationType> authorizationTypes = new ArrayList<AuthorizationType>();
-		java.util.Map<Class<?>, Resource> resources = apiSource.getValidResources();
+		List<AuthorizationType> authorizationTypes = new ArrayList<>();
+		Map<Class<?>, Resource> resources = apiSource.getValidResources();
 		for (Class<?> key : resources.keySet()) {
 			ApiListing doc;
 			try {
@@ -91,56 +93,7 @@ public class MavenDocumentSource extends AbstractDocumentSource {
         JaxrsApiReader reader = new DefaultJaxrsApiReader();
 		ApiListing apiListing = reader.read(basePath, resource.getControllerClass(), swaggerConfig).get();
 
-		List<ApiDescription> apiDescriptions = new ArrayList<>();
-		scala.collection.immutable.List<ApiDescription> apis = apiListing.apis();
-		List<ApiDescription> apiDescriptionsJava = JavaConversions.asJavaList(apis);
-
-		for(ApiDescription apiDescription : apiDescriptionsJava) {
-			List<Operation> operationsJava = JavaConversions.asJavaList(apiDescription.operations());
-			for(String key : resource.getOperationsUris()) {
-				List<Operation> operations = new ArrayList<>();
-				for(RouteMethod routeMethod : resource.getRouteMethodsFor(key)) {
-					for(Operation operation : operationsJava) {
-						if(routeMethod.getControllerMethod().getName().equals(operation.nickname())){
-
-							scala.collection.immutable.List<Parameter> parameters = operation.parameters();
-							List<Parameter> resultParameters = new ArrayList<>();
-							List<Parameter> parametersJava = JavaConversions.asJavaList(parameters);
-							java.lang.reflect.Parameter[] methodParameters = routeMethod.getControllerMethod().getParameters();
-
-							for (int i = 0; i < methodParameters.length; i++) {
-								java.lang.reflect.Parameter methodParameter = methodParameters[i];
-								Parameter parameter = parametersJava.get(i);
-								ParameterSpecification parameterSpecification = new ParameterSpecification(methodParameter, parameter);
-
-								resultParameters.add(new Parameter(parameterSpecification.getParameterName(), parameter.description(), parameter.defaultValue(),
-										parameter.required(), parameter.allowMultiple(), parameter.dataType(),
-										parameter.allowableValues(), parameterSpecification.getParameterType(), parameter.paramAccess()));
-							}
-
-							String httpMethod = routeMethod.getHttpMethod();
-							operations.add(new Operation(httpMethod, operation.summary(), operation.notes(),
-									operation.responseClass(), operation.nickname(), operation.position(), operation.produces(),
-									operation.consumes(), operation.protocols(), operation.authorizations(),
-									scala.collection.immutable.List.fromIterator(JavaConversions.asScalaIterator(resultParameters.iterator())),
-									operation.responseMessages(), operation.deprecated()));
-						}
-					}
-				}
-				ApiDescription newApiDescription = new ApiDescription(key, Option.empty(),
-						scala.collection.immutable.List.fromIterator(JavaConversions.asScalaIterator(operations.iterator())));
-				apiDescriptions.add(newApiDescription);
-			}
-		}
-
-		apis = scala.collection.immutable.List.fromIterator(JavaConversions.asScalaIterator(apiDescriptions.iterator()));
-
-		apiListing = new ApiListing(swaggerConfig.getApiVersion(), apiListing.swaggerVersion(),
-				basePath + apiSource.getApiUri(), resource.getResourceUri(), apiListing.produces(), apiListing.consumes(),
-				apiListing.protocols(), apiListing.authorizations(), apis, apiListing.models(), apiListing.description(),
-				apiListing.position());
-
-		if (None.canEqual(apiListing)) return null;
+		apiListing = modelMapper.changeJaxrsApiListingBasingOnNinjaValues(apiListing, resource, swaggerConfig, basePath + apiSource.getApiUri());
 
 		return apiListing;
 	}
---
+++ src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/NinjaToSwaggerModelMapper.java
@@ -0,0 +1,97 @@
+package com.github.kongchen.swagger.docgen.mavenplugin;
+
+import com.wordnik.swagger.config.SwaggerConfig;
+import com.wordnik.swagger.model.ApiDescription;
+import com.wordnik.swagger.model.ApiListing;
+import com.wordnik.swagger.model.Operation;
+import com.wordnik.swagger.model.Parameter;
+import scala.None;
+import scala.Option;
+import scala.collection.JavaConversions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NinjaToSwaggerModelMapper {
+
+	public ApiListing changeJaxrsApiListingBasingOnNinjaValues(ApiListing apiListing, Resource resource,
+			SwaggerConfig swaggerConfig, String basePath) {
+
+		scala.collection.immutable.List<ApiDescription> apis = getApiDescriptionsWithNinjaValues(apiListing, resource);
+
+		apiListing = new ApiListing(swaggerConfig.getApiVersion(), apiListing.swaggerVersion(),
+				basePath, resource.getResourceUri(), apiListing.produces(), apiListing.consumes(),
+				apiListing.protocols(), apiListing.authorizations(), apis, apiListing.models(), apiListing.description(),
+				apiListing.position());
+
+		if (None.canEqual(apiListing)) return null;
+
+		return apiListing;
+	}
+
+	private scala.collection.immutable.List<ApiDescription> getApiDescriptionsWithNinjaValues(ApiListing apiListing,
+			Resource resource) {
+		List<ApiDescription> resultApiDescriptions = new ArrayList<>();
+
+		scala.collection.immutable.List<ApiDescription> apis = apiListing.apis();
+		List<ApiDescription> apiDescriptions = JavaConversions.asJavaList(apis);
+
+		for(ApiDescription apiDescription : apiDescriptions) {
+			fillUpApiDescriptionWithNinjaValues(apiDescription, resource, resultApiDescriptions);
+		}
+
+		return scala.collection.immutable.List.fromIterator(JavaConversions.asScalaIterator(resultApiDescriptions.iterator()));
+	}
+
+	private void fillUpApiDescriptionWithNinjaValues(ApiDescription apiDescription, Resource resource, List<ApiDescription> resultApiDescriptions) {
+		List<Operation> operations = JavaConversions.asJavaList(apiDescription.operations());
+		for(String operationUri : resource.getOperationsUris()) {
+			List<Operation> resultOperations = fillUpOperationsWithNinjaValues(resource, operations, operationUri);
+			ApiDescription newApiDescription = new ApiDescription(operationUri, Option.empty(),
+					scala.collection.immutable.List.fromIterator(JavaConversions.asScalaIterator(resultOperations.iterator())));
+			resultApiDescriptions.add(newApiDescription);
+		}
+	}
+
+	private List<Operation> fillUpOperationsWithNinjaValues(Resource resource, List<Operation> operations, String operationUri) {
+		List<Operation> resultOperations = new ArrayList<>();
+		for(RouteMethod routeMethod : resource.getRouteMethodsFor(operationUri)) {
+			for(Operation operation : operations) {
+				addOperationWhenEquivalentsInModelsWereFound(operation, routeMethod, resultOperations);
+			}
+		}
+		return resultOperations;
+	}
+
+	private void addOperationWhenEquivalentsInModelsWereFound(Operation operation, RouteMethod routeMethod, List<Operation> resultOperations) {
+		boolean equivalentsWereFound = routeMethod.getControllerMethod().getName().equals(operation.nickname());
+		if(equivalentsWereFound){
+
+			List<Parameter> resultParameters = changeOperationParametersToNinjaSpecific(operation, routeMethod);
+
+			resultOperations.add(new Operation(routeMethod.getHttpMethod(), operation.summary(), operation.notes(),
+					operation.responseClass(), operation.nickname(), operation.position(), operation.produces(),
+					operation.consumes(), operation.protocols(), operation.authorizations(),
+					scala.collection.immutable.List.fromIterator(JavaConversions.asScalaIterator(resultParameters.iterator())),
+					operation.responseMessages(), operation.deprecated()));
+		}
+	}
+
+	private List<Parameter> changeOperationParametersToNinjaSpecific(Operation operation, RouteMethod routeMethod) {
+		List<Parameter> resultParameters = new ArrayList<>();
+		List<Parameter> parameters = JavaConversions.asJavaList(operation.parameters());
+		java.lang.reflect.Parameter[] methodParameters = routeMethod.getControllerMethod().getParameters();
+
+		for (int i = 0; i < methodParameters.length; i++) {
+			java.lang.reflect.Parameter methodParameter = methodParameters[i];
+			Parameter parameter = parameters.get(i);
+			ParameterSpecification parameterSpecification = new ParameterSpecification(methodParameter, parameter);
+
+			resultParameters.add(new Parameter(parameterSpecification.getParameterName(), parameter.description(), parameter.defaultValue(),
+					parameter.required(), parameter.allowMultiple(), parameter.dataType(),
+					parameter.allowableValues(), parameterSpecification.getParameterType(), parameter.paramAccess()));
+		}
+
+		return resultParameters;
+	}
+}
